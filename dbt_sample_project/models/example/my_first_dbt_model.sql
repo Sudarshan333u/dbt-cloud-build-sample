fd3b569b30dbc6ff222809249b4cd06a60ce6360
sudarshan66degrees <sudarshan.c@66degrees.com>
2025-05-14 13:36:26 +0530

    added repobuild.yaml

--- dbt_sample_project/models/example/my_first_dbt_model.sql
+++ dbt_sample_project/models/example/my_first_dbt_model.sql
@@ -4,6 +4,6 @@
 SELECT
   greeting_text,
   run_timestamp
-FROM {{ source('dbt_sample_data', 'my_first_dbt_model') }}
+FROM {{ source('dbt_sample_data', 'my_first_dbt_model2') }}
 WHERE
   greeting_text IS NOT NULL
